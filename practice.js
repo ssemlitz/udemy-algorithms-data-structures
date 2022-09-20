b458ce04496809a7351065743743cde966af7d68
ssemlitz <sdsemlitz@yahoo.com>
2022-09-20 15:43:37 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+// function addUpTo(n) {
+//   let total = 0; 
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+function addUpTo(n) {
+  return n * (n+1)/2
+}
+
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+console.log((t2-t1)/1000)
+
+addUpTo(10)
