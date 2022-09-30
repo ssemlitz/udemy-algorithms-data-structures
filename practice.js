458fa9250b5098f7ecaac74786be88673d89e33c
ssemlitz <sdsemlitz@yahoo.com>
2022-09-30 15:12:31 -0400

    linear search practice

--- practice.js
+++ practice.js
@@ -124,3 +124,10 @@
  
 //   return minLen === Infinity ? 0 : minLen;
 // }
+
+function linearSearch(arr, val) {
+  for (let i=0; i<arr.length;i++) {
+    if (arr[i] === val) return i
+  }
+  return -1
+}
