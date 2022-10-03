6c1f2067135ec0510b0303d352f305f730d3b4fe
ssemlitz <sdsemlitz@yahoo.com>
2022-10-03 15:45:58 -0400

    bubble sort optimized

--- practice.js
+++ practice.js
@@ -133,18 +133,23 @@
 // }
 
 function bubbleSort(arr) {
-  for (let i=0;i<arr.length;i++) {
-    for (let j=0;j<arr.length;j++) {
+  let noSwaps
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true
+    for (let j = 0; j < i-1; j++) {
       console.log(arr, arr[j], arr[j+1])
-      if (arr[j] > arr[i]) {
+      if (arr[j] > arr[j+1]) {
         //SWAP!
         let temp = arr[j]
         arr[j] = arr[j+1]
         arr[j+1] = temp
+        noSwaps = false
       }
     }
+    if (noSwaps) break
   }
+  console.log(arr)
   return arr
 }
 
-bubbleSort([37, 45, 29, 8])
+bubbleSort([37, 45, 29, 8, 12, 88, -3])
