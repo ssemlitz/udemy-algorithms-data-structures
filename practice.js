b3671e68251439069d7c8671ba73b8069fa06be4
ssemlitz <sdsemlitz@yahoo.com>
2022-10-06 15:48:37 -0400

    radix sort

--- practice.js
+++ practice.js
@@ -241,32 +241,65 @@
 
 // mergeSort([10,24,76,73])
 
-function pivot(arr, start=0, end=arr.length-1){
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-  }
+// function pivot(arr, start=0, end=arr.length-1){
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+//   }
   
-  let pivot = arr[start]
-  let swapIdx = start
+//   let pivot = arr[start]
+//   let swapIdx = start
 
-  for(let i = start + 1; i < arr.length; i++) {
-    if(pivot > arr[i]){
-      swapIdx++
-      swap(arr, swapIdx, i)
+//   for(let i = start + 1; i < arr.length; i++) {
+//     if(pivot > arr[i]){
+//       swapIdx++
+//       swap(arr, swapIdx, i)
+//     }
+//   }
+//   swap(arr, start, swapIdx)
+//   // console.log(swapIdx)
+//   return swapIdx
+// }
+
+// function quickSort(arr, left = 0, right = arr.length - 1){
+//   if (left < right) {
+//     let pivotIndex = pivot(arr, left, right)
+//     quickSort(arr, left, pivotIndex - 1)
+//     quickSort(arr, pivotIndex + 1, right)
+//   }
+//   return arr
+// }
+
+// quickSort([4,8,2,1,5,7,6,3])
+
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+}
+
+function digitCount(num) {
+  if (num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0
+  for (let i=0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+function radixSort(nums){
+  let maxDigitCount = mostDigits(nums)
+  for (let k = 0; k < maxDigitCount; k++){
+    let digitBuckets = Array.from({length: 10}, () => [])
+    for (let i = 0; i < nums.length;i++){
+      let digit = getDigit(nums[i], k)
+      digitBuckets[digit].push(nums[i])
     }
+    nums = [].concat(...digitBuckets)
   }
-  swap(arr, start, swapIdx)
-  console.log(swapIdx)
-  return swapIdx
+  console.log(nums)
+  return nums
 }
 
-function quickSort(arr, left = 0, right = arr.length - 1){
-  if (left < right) {
-    let pivotIndex = pivot(arr, left, right)
-    quickSort(arr, left, pivotIndex - 1)
-    quickSort(arr, pivotIndex + 1, right)
-  }
-  return arr
-}
-
-pivot([4,8,2,1,5,7,6,3])
+radixSort([12, 2435, 75436, 32, 1, 6, 45363, 34])
