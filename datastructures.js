d7080ddd0e124e9a970d8e932f32d75aa0a75e96
ssemlitz <sdsemlitz@yahoo.com>
2022-10-14 15:39:46 -0400

    singly linked list insert

--- datastructures.js
+++ datastructures.js
@@ -83,6 +83,18 @@ class SinglyLinkedList{
     }
     return false
   }
+  insert(index, val){
+    if (index < 0 || index > this.length) return false
+    if (index === this.length) return !!this.push(val)
+    if (index === 0) return !!this.unshift(val)
+    let newNode = new Node(val)
+    let prev = this.get(index-1)
+    let temp = prev.next
+    prev.next = newNode
+    newNode.next = temp
+    this.length++
+    return true
+  }
 }
 
 // let first = new Node("Hi")
