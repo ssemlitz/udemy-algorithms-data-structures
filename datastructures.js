39ed5b8ec369f592c2153c52ad8247d11ccc06f6
ssemlitz <sdsemlitz@yahoo.com>
2022-10-14 15:17:03 -0400

    singly linked list unshift

--- datastructures.js
+++ datastructures.js
@@ -53,6 +53,18 @@ class SinglyLinkedList{
     }
     return currentHead
   }
+  unshift(val){
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      newNode.next = this.head 
+      this.head = newNode
+    }
+    this.length++
+    return this
+  }
 }
 
 // let first = new Node("Hi")
