bf34190be1ebbffb0c2a333befcf430f4ae2af22
ssemlitz <sdsemlitz@yahoo.com>
2022-10-14 15:56:19 -0400

    singly linked list reverse

--- datastructures.js
+++ datastructures.js
@@ -107,6 +107,29 @@ class SinglyLinkedList{
     this.length--
     return removed
   }
+  print(){
+    let arr = []
+    let current = this.head
+    while (current){
+      arr.push(current.val)
+      current = current.next
+    }
+    console.log(arr)
+  }
+  reverse(){
+    let node = this.head
+    this.head = this.tail
+    this.tail = node
+    let next
+    let prev = null
+    for(let i = 0; i < this.length; i++){
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this
+  }
 }
 
 // let first = new Node("Hi")
