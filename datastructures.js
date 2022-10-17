b914ee6d026a3497b873c5c25c687e6c890510c8
ssemlitz <sdsemlitz@yahoo.com>
2022-10-17 14:56:25 -0400

    doublylinkedlist set

--- datastructures.js
+++ datastructures.js
@@ -238,4 +238,14 @@ class DoublyLinkedList {
     }
     return current
   }
+
+  set(index, val){
+    let foundNode = this.get(index)
+    if (foundNode !== null){
+      foundNode.val = val
+      return true
+    }
+    return false
+  }
+
 }
