d84702fc7f1a94d5c0d598182d9d48c8fecf5f84
ssemlitz <sdsemlitz@yahoo.com>
2022-10-17 15:05:13 -0400

    doublylinkedlist insert

--- datastructures.js
+++ datastructures.js
@@ -248,4 +248,21 @@ class DoublyLinkedList {
     return false
   }
 
+  insert(index, val){
+    if (index < 0 || index > this.length) return false
+    if (index === 0) return !!this.unshift(val)
+    if (index === this.length) return !!this.push(val)
+
+    let newNode = new Node(val)
+    let beforeNode = this.get(index - 1)
+    let afterNode = beforeNode.next
+    
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    afterNode.prev = newNode
+    newNode.next = afterNode
+    this.length++
+    return true
+  }
+
 }
