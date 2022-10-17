d941d819de8dfa09d51a104c81f423edc21c9fd9
ssemlitz <sdsemlitz@yahoo.com>
2022-10-17 14:37:53 -0400

    doublylinkedlist shift

--- datastructures.js
+++ datastructures.js
@@ -188,7 +188,21 @@ class DoublyLinkedList {
     }
     this.length--
     return poppedNode
+  }
 
+  shift(){
+    if(!this.head) return undefined
+    let oldHead = this.head
+    if (this.length === 1){
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = oldHead.next
+      this.head.prev = null
+      oldHead.next = null
+    }
+    this.length--
+    return oldHead
   }
 
 }
