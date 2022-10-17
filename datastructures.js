b70765e95bd0979547e8968cea07a702c11eb7b7
ssemlitz <sdsemlitz@yahoo.com>
2022-10-17 14:40:48 -0400

    doublylinkedlist unshift

--- datastructures.js
+++ datastructures.js
@@ -205,4 +205,17 @@ class DoublyLinkedList {
     return oldHead
   }
 
+  unshift(val){
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.head.prev = newNode
+      newNode.next = this.head
+      this.head = newNode
+    }
+    this.length++
+    return this
+  }
 }
