9e0f1381a61f0de35129a6e0c59d922b3f0a8209
ssemlitz <sdsemlitz@yahoo.com>
2022-10-20 11:56:48 -0400

    BST insert

--- datastructures.js
+++ datastructures.js
@@ -284,73 +284,114 @@
 //   }
 
 // }
+// class Node {
+//   constructor(value){
+//     this.value = value
+//     this.next = null
+//   }
+// }
+
+// class Stack {
+//   constructor(){
+//     this.first = null
+//     this.last = null
+//     this.size = 0
+//   }
+
+//   push(val){
+//     let newNode = new Node(val)
+//     if (!this.first){
+//       this.first = newNode
+//       this.last = newNode
+//     } else {
+//       let temp = this.first
+//       this.first = newNode
+//       this.first.next = temp
+//     }
+//     return ++this.size
+//   }
+
+//   pop(){
+//     if (!this.first) return null
+//     let temp = this.first
+//     if (this.first === this.last){
+//       this.last = null
+//     }
+//     this.first = this.first.next
+//     this.size--
+//     return temp.value
+//   }
+// }
+
+// class Queue {
+//   constructor(){
+//     this.first = null
+//     this.last = null
+//     this.size = 0
+//   }
+
+//   enqueue(val){
+//     let newNode = new Node(val)
+//     if(!this.first){
+//       this.first = newNode
+//       this.last = newNode
+//     } else {
+//       this.last.next = newNode
+//       this.last = newNode
+//     }
+//     return ++this.size
+//   }    
+
+//   dequeue(){
+//     if (!this.first) return null
+//     let temp = this.first
+//     if(this.first === this.last){
+//       this.last = null
+//     }
+//     this.first = this.first.next
+//     this.size--
+//     return temp.value
+//   }
+// }
+
 class Node {
   constructor(value){
     this.value = value
-    this.next = null
+    this.left = null
+    this.right = null
   }
 }
 
-class Stack {
+class BinarySearchTree {
   constructor(){
-    this.first = null
-    this.last = null
-    this.size = 0
+    this.root = null
   }
 
-  push(val){
-    let newNode = new Node(val)
-    if (!this.first){
-      this.first = newNode
-      this.last = newNode
+  insert(value){
+    let newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+      return this
     } else {
-      let temp = this.first
-      this.first = newNode
-      this.first.next = temp
+      let current = this.root
+      while(true){
+        if(value === current.value) return undefined
+        if(value < current.value){
+          if(current.left === null){
+            current.left = newNode
+            return this
+          } else {
+            current = current.left
+          }
+        } else if (value > current.value){
+          if (current.right === null){
+            current.right = newNode
+            return this
+          } else {
+            current = current.right
+          }
+        }
+      }
     }
-    return ++this.size
-  }
-
-  pop(){
-    if (!this.first) return null
-    let temp = this.first
-    if (this.first === this.last){
-      this.last = null
-    }
-    this.first = this.first.next
-    this.size--
-    return temp.value
   }
 }
-
-class Queue {
-  constructor(){
-    this.first = null
-    this.last = null
-    this.size = 0
-  }
-
-  enqueue(val){
-    let newNode = new Node(val)
-    if(!this.first){
-      this.first = newNode
-      this.last = newNode
-    } else {
-      this.last.next = newNode
-      this.last = newNode
-    }
-    return ++this.size
-  }    
-
-  dequeue(){
-    if (!this.first) return null
-    let temp = this.first
-    if(this.first === this.last){
-      this.last = null
-    }
-    this.first = this.first.next
-    this.size--
-    return temp.value
-  }
-}
-
