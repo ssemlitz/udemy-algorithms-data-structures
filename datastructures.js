4095c096fd5e6a544a3756e0244d43697a4d0d2e
ssemlitz <sdsemlitz@yahoo.com>
2022-10-24 14:44:21 -0400

    BST BFS

--- datastructures.js
+++ datastructures.js
@@ -411,4 +411,21 @@ class BinarySearchTree {
     if(!found) return undefined
     return current
   }
+  BFS(){
+    let data = []
+    let queue = []
+    let node = this.root
+
+    queue.push(node)
+    while(queue.length){
+      node = queue.shift()
+      data.push(node)
+      if(node.left) queue.push(node.left)
+      if(node.right) queue.push(node.right)
+    }
+    return data
+  }
+  DFSPreoOrder(){
+    
+  }
 }
