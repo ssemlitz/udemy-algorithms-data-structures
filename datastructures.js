fbc5f7fa7a1b008e06dfd239e573be3825d00f12
ssemlitz <sdsemlitz@yahoo.com>
2022-10-25 10:40:50 -0400

    MaxBinaryHeap insert

--- datastructures.js
+++ datastructures.js
@@ -468,6 +468,29 @@ tree.insert(8)
 tree.insert(15)
 tree.insert(20)
 
-console.log(tree.DFSPreOrder())
-console.log(tree.DFSPostOrder())
-console.log(tree.DFSInOrder())
+class MaxBinaryHeap {
+  constructor(){
+    this.values = [41,39,33,18,27,12]
+  }
+  insert(element){
+    this.values.push(element)
+    this.bubbleUp()
+  }
+  bubbleUp(){
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+    while(idx > 0){
+      let parentIdx = Math.floor((idx - 1)/2)
+      let parent = this.values[parentIdx]
+      if(element <= parent) break
+      this.values[parentIdx] = element
+      this.values[idx] = parent
+      idx = parentIdx
+      
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
+console.log(heap)
