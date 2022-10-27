2b92e91c3ed6986b6a62755cf65219976ae8cad4
ssemlitz <sdsemlitz@yahoo.com>
2022-10-27 11:46:00 -0400

    graph removerVertex

--- datastructures.js
+++ datastructures.js
@@ -704,7 +704,11 @@ class Graph {
   }
 
   removeVertex(vertex){
-    
+    while(this.adjacencyList[vertex].length){
+      const adjacentVertex = this.adjacencyList[vertex].pop()
+      this.removeEdge(vertex, adjacentVertex)
+    }
+    delete this.adjacencyList[vertex]
   }
 }
 
