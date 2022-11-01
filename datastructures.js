4b8554579ff40674526ece3bdcdb07742f4e16de
ssemlitz <sdsemlitz@yahoo.com>
2022-11-01 15:34:06 -0400

    graph DFS recursive

--- datastructures.js
+++ datastructures.js
@@ -710,6 +710,41 @@ class Graph {
     }
     delete this.adjacencyList[vertex]
   }
+
+  depthFirstRecursive(start){
+    let result = []
+    let visited = {}
+    let adjacencyList = this.adjacencyList
+    
+    function dfs(vertex){
+      if (!vertex) return null
+      visited[vertex] = true
+      result.push(vertex)
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]) {
+          return dfs(neighbor)
+        }
+      })
+    }
+    dfs(start)
+    return result
+  }
 }
 
+let g = new Graph()
 
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+
+console.log(g.depthFirstRecursive("A"))
