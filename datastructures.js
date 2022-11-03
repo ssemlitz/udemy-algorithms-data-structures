26bdf0bb688530078353eb843202cf6c175c4796
ssemlitz <sdsemlitz@yahoo.com>
2022-11-03 12:10:33 -0400

    dynamic programming fib sequence

--- datastructures.js
+++ datastructures.js
@@ -1110,3 +1110,19 @@ graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
 graph.Dijkstra("A", "E");
+
+
+// function fib(n){
+//   if(n <= 2) return 1
+//   return fib(n-1) + fib(n-2)
+// }
+
+function fib(n, memo = []){
+  if(memo[n] !== undefined) return memo[n]
+  if(n <= 2) return 1
+  let res = fib(n-1, memo) + fib(n-2, memo)
+  memo[n] = res
+  return res
+}
+
+console.log(fib(100))
