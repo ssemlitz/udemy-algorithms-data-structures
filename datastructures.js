a472b36331dc5909413ee1a3a10a5669866568cc
ssemlitz <sdsemlitz@yahoo.com>
2022-11-03 12:16:47 -0400

    fib_table

--- datastructures.js
+++ datastructures.js
@@ -1117,7 +1117,7 @@ graph.Dijkstra("A", "E");
 //   return fib(n-1) + fib(n-2)
 // }
 
-function fib(n, memo = []){
+function fib_memo(n, memo = []){
   if(memo[n] !== undefined) return memo[n]
   if(n <= 2) return 1
   let res = fib(n-1, memo) + fib(n-2, memo)
@@ -1125,4 +1125,13 @@ function fib(n, memo = []){
   return res
 }
 
-console.log(fib(100))
+function fib_table(n){
+  if(n <= 2) return 1
+  let fibNums = [0,1,1]
+  for (let i = 3; i <= n; i++){
+    fibNums[i] = fibNums[i-1] + fibNums[i-2]
+  }
+  return fibNums[n]
+}
+
+console.log(fib_table(100))
